7e8aa6afe624848cafdbe5948d831dac62d127da
arjunarjun27 <arjunarjun27@gmail.com>
2018-06-30 13:00:50 +0530

    siimple generics example

---
+++ src/javareferencepractice/Gen.java
@@ -0,0 +1,22 @@
+package javareferencepractice;
+
+public class Gen<T> {
+	
+	T ob;
+
+	public Gen(T ob) {
+		this.ob = ob;
+	}
+	
+	
+	public T getOb() {
+		return ob;
+	}
+
+	
+	public void showType(){
+		
+		System.out.println("Type of T is "+ob.getClass());
+	}
+	
+}
--- src/javareferencepractice/MainApp.java
+++ src/javareferencepractice/MainApp.java
@@ -5,6 +5,20 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		Gen<Integer> iob; 
+		iob=new Gen<Integer>(50);
+		iob.showType();
+		System.out.println(iob.getOb());
+		
+		
+		Gen<String> strobj;
+		strobj=new Gen<String>("arjun"); 
+		strobj.showType();
+		System.out.println(strobj.getOb());
+		
+	//	iob=strobj; //not possible
+
 	}
 
 }
